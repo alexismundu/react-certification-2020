3fe5321e8eddca36513e049ec95f40bd2c6d0d42
Alexis Munoz <alexis.munoz@wizeline.com>
2021-04-21 23:27:59 -0500

    Add Liked videos to local storage

--- src/components/VideoDetailsDescription/VideoDetailsDescription.component.jsx
+++ src/components/VideoDetailsDescription/VideoDetailsDescription.component.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 
 import { VideoDescription } from './VideoDetailsDescription.styled';
 import { destructDate } from '../../utils/datetime';
+import { isElementInArray, checkInFavorites } from '../../utils/fns';
 
 const formatDate = (date) => {
   const { year, month, day } = destructDate(new Date(date));
@@ -10,15 +12,38 @@ const formatDate = (date) => {
 };
 
 const VideoDetailsDescription = ({ videoId, title, description, publishedAt }) => {
-  const handleAddToFavorites = () => {
-    console.log('Added to favorites ', title, videoId);
-  };
+  const [isVideoLiked, setIsVideoLiked] = useState(false);
+
+  useEffect(() => {
+    setIsVideoLiked(checkInFavorites(videoId));
+  }, [videoId]);
+
+  function handleAddToFavorites() {
+    setIsVideoLiked(!isVideoLiked);
+    let newFavoriteVideos = [];
+    const favoriteVideos = JSON.parse(window.localStorage.getItem('favoriteVideos'));
+    const isArrayInitialized = favoriteVideos !== null && favoriteVideos.length > 0;
+    let shouldRemoveElement = false;
+    if (!isArrayInitialized) {
+      newFavoriteVideos.push(videoId);
+    } else {
+      shouldRemoveElement = isElementInArray(favoriteVideos, videoId);
+      if (shouldRemoveElement) {
+        newFavoriteVideos = favoriteVideos.filter((value) => value !== videoId);
+      } else {
+        favoriteVideos.push(videoId);
+        newFavoriteVideos = favoriteVideos;
+      }
+    }
+
+    window.localStorage.setItem('favoriteVideos', JSON.stringify(newFavoriteVideos));
+  }
 
   return (
     <>
       <h1>{title}</h1>
       <button type="button" onClick={handleAddToFavorites}>
-        <FavoriteIcon />
+        {isVideoLiked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
       </button>
       <h6>{formatDate(publishedAt)}</h6>
       <VideoDescription>{description}</VideoDescription>
--- src/utils/fns.js
+++ src/utils/fns.js
@@ -2,4 +2,14 @@ function random(limit) {
   return Math.floor(Math.random() * limit);
 }
 
-export { random };
+function isElementInArray(arr, e) {
+  return Boolean(arr.find((tmp) => tmp === e));
+}
+
+function checkInFavorites(videoId) {
+  const favoriteVideos = JSON.parse(window.localStorage.getItem('favoriteVideos'));
+  if (favoriteVideos === null) return false;
+  return isElementInArray(favoriteVideos, videoId);
+}
+
+export { random, isElementInArray, checkInFavorites };

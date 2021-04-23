83365cf4bff4f7c673939e8aab0fbc9b18323c08
Alexis Munoz <alexis.munoz@wizeline.com>
2021-04-22 19:22:08 -0500

    Fix height of layout so the background covers all the page views

--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Main = styled.main`
   width: 100%;
-  height: 100%;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -10,6 +10,7 @@ export const Main = styled.main`
 `;
 
 export const GlobalWrapper = styled.div`
+  height: 100%;
   background-color: ${(props) => props.theme.bg_color};
   color: ${(props) => props.theme.text_color};
 `;
